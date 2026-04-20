d4b080ba2db6494d377fd47265e5a17df92b8743
glennthielman <37749736+glennthielman@users.noreply.github.com>
2026-04-20 22:08:17 +0200

    Use lombok for equals and hash

--- src/main/java/be/keleos/dynamicwebscraper/adaptor/eneco/model/OutPriceRecord.java
+++ src/main/java/be/keleos/dynamicwebscraper/adaptor/eneco/model/OutPriceRecord.java
@@ -1,6 +1,7 @@
 package be.keleos.dynamicwebscraper.adaptor.eneco.model;
 
 import be.keleos.dynamicwebscraper.model.Price;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -9,16 +10,16 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Objects;
 
 @Accessors(chain = true)
 @Getter
 @Setter
-
+@EqualsAndHashCode
 public class OutPriceRecord {
 
     private LocalDate date;
     private LocalTime time;
+    @EqualsAndHashCode.Exclude
     private BigDecimal price;
 
     public Price toPrice() {
@@ -26,18 +27,5 @@ public class OutPriceRecord {
                 .setStartTime(LocalDateTime.of(date, time))
                 .setEndTime(LocalDateTime.of(date, time).plusHours(1))
                 .setPrice(price);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(date, time, price);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if(this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        var priceRecord = (OutPriceRecord) o;
-        return this.date.isEqual(priceRecord.date) && this.time.equals(priceRecord.getTime());
     }
 }
